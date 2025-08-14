073df1209640689adf1a1fd1612214fdd2ad72ab
Diya-1607 <aartipatil11014@gmail.com>
2025-08-14 13:39:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Get-Well-Soon/',
   plugins: [react(), tailwindcss(),],
+    base: '/GetWellSoon/',
 })
